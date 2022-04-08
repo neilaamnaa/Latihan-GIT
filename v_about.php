9914bf3179f06f94b4e19cc92b7054f6ea038d9e
neilaamnaa <101781333+neilaamnaa@users.noreply.github.com>
2022-04-08 18:35:14 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,21 @@
+<section>
+	<h1><?php echo $judul ?></h1>
+	<h4>Nama</h4>
+	<ul type="disc">
+		<li>Nama Depan : Neila</li>
+		<li>Nama Belakang : Amna</li>
+	</ul>
+	<br>
+	<h4>Alamat</h4>
+	<ul type="none">
+		<li>Jl Kiunara RT 02/05 NO. 121 Cileungsi Bogor</li>
+	</ul>
+	<h4>Tempat Lahir</h4>
+	<ul type="none">
+		<li>Bogor</li>
+	</ul>
+	<h4>Olahraga Favorit</h4>
+	<ul type="square">
+		<li>Bulutangkis</li>
+		<li>Catur</li>
+</section>
